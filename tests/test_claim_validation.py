f97aa4d49e46902d772bd0014b1c8dfbd485a449
pkbhuvan <pkalyanbhuvan@gmail.com>
2026-08-04 20:40:15 +0530

    DataFrame creation in test

--- tests/test_claim_validation.py
+++ tests/test_claim_validation.py
@@ -2,13 +2,7 @@ import pandas as pd
 from src.validation.claim_validation import remove_invalid_claims
 def test_remove_invalid_claims():
 
-    df = pd.DataFrame({
-        "claim_number": [
-            "ABC123",
-            None,
-            "XYZ456"
-        ]
-    })
+    df = pd.DataFrame({"claim_number": ["ABC123",None,"XYZ456" ]})
 
     result = remove_invalid_claims(df)
 
