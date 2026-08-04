1fe18a1f38fcfb18f067e808aa9fda220f030d6a
pkbhuvan <pkalyanbhuvan@gmail.com>
2026-08-04 17:32:02 +0000

    file updater

--- src/utils/file_reader.py
+++ src/utils/file_reader.py
@@ -4,25 +4,52 @@ import pandas as pd
 
 def read_membership_files():
 
-    files = glob.glob("data/raw/Patient-membership-*.xlsx")
+    files = glob.glob(
+        "data/raw/Patient-membership-*.xlsx"
+    )
 
     dfs = []
 
     for file in files:
-        print(f"Reading {file}")
-        dfs.append(pd.read_excel(file))
 
-    return pd.concat(dfs, ignore_index=True)
+        print(f"Reading {file}")
+
+        df = pd.read_excel(
+            file,
+            dtype=str
+        )
+
+        dfs.append(df)
+
+
+    return pd.concat(
+        dfs,
+        ignore_index=True
+    )
+
 
 
 def read_claim_files():
 
-    files = glob.glob("data/raw/Patient-claim-*.xlsx")
+    files = glob.glob(
+        "data/raw/Patient-claim-*.xlsx"
+    )
 
     dfs = []
 
     for file in files:
-        print(f"Reading {file}")
-        dfs.append(pd.read_excel(file))
 
-    return pd.concat(dfs, ignore_index=True)
+        print(f"Reading {file}")
+
+        df = pd.read_excel(
+            file,
+            dtype=str
+        )
+
+        dfs.append(df)
+
+
+    return pd.concat(
+        dfs,
+        ignore_index=True
+    )
